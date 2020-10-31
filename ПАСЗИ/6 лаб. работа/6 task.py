3b37a3d0280d4d0c74df18c65cf3929f374000e0
alfisnig <valerasafin2005@gmail.com>
2020-10-31 16:59:41 +0300

    добавил код для 6 лаб. работы

---
+++ ПАСЗИ/6 лаб. работа/6 task.py
@@ -0,0 +1,47 @@
+p = 991
+n = 3
+alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+
+def generatePolynomial(n: int, x: int):
+    polynomialValue = 0
+    for i, digit in enumerate(range(n, 0, -1), start=1):
+        polynomialValue += i * (x ** digit)
+    polynomialValue += n + 1
+    polynomialValue %= p
+    return polynomialValue
+
+
+while True:
+    choice = input("1 - Хочу зашировать текст\n2 - Хочу расшифровать текст\n")
+    if choice == "1" or choice == "2":
+        break
+
+choice = int(choice)
+if choice == 1:
+    mes = open("plain_text.txt", encoding='utf-8').read().strip()
+
+    plainMessage = []
+
+    for symbol in mes:
+        position = alphabet.find(symbol.lower())
+        if position > -1:
+            plainMessage.append(str(generatePolynomial(n, position + 1)))
+        else:
+            plainMessage.append(symbol)
+
+    open("crypted_text.txt", "w", encoding='utf-8').write(" ".join(plainMessage))
+elif choice == 2:
+    mes = open("crypted_text.txt").read().strip()
+
+    decryptedMessage = ""
+    for num in mes.split(" "):
+        if not num.isdigit():
+            decryptedMessage += num
+            continue
+        num = int(num)
+        for letterPos in range(len(alphabet)):
+            if num == generatePolynomial(n, letterPos + 1):
+                decryptedMessage += alphabet[letterPos]
+
+    open("decrypted_text.txt", "w", encoding='utf-8').write("".join(decryptedMessage))
+
